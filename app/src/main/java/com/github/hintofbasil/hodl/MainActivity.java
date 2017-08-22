62a35c5fd45feb5bfc31a56e2e294b08cfcb805b
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2017-08-22 13:07:56 +0100

    Put coin market cap updater into intent service

--- app/src/main/java/com/github/hintofbasil/hodl/MainActivity.java
+++ app/src/main/java/com/github/hintofbasil/hodl/MainActivity.java
@@ -1,7 +1,10 @@
 package com.github.hintofbasil.hodl;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -16,14 +19,9 @@ import android.widget.Toast;
 
 import com.github.hintofbasil.hodl.coinSummaryList.CoinSummary;
 import com.github.hintofbasil.hodl.coinSummaryList.CoinSummaryListAdapter;
+import com.github.hintofbasil.hodl.database.CoinMarketCapUpdaterService;
 import com.github.hintofbasil.hodl.database.CoinSummaryDbHelper;
 import com.github.hintofbasil.hodl.database.CoinSummarySchema;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -32,11 +30,9 @@ import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
 import java.math.BigDecimal;
 
-import cz.msebera.android.httpclient.Header;
-
 public class MainActivity extends Activity {
 
-    public static final String COIN_MARKET_CAP_API_URL = "https://api.coinmarketcap.com/v1/ticker/?limit=100";
+    public static final String MAIN_ACTIVITY_REFRESH = "MAIN_ACTIVITY_REFRESH";
 
     private TextView totalCoinSummary;
     private SwipeRefreshLayout swipeRefreshLayout;
@@ -49,6 +45,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         initImageLoader();
         setContentView(R.layout.activity_main);
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(MAIN_ACTIVITY_REFRESH);
+        intentFilter.addAction(CoinMarketCapUpdaterService.STATUS_FAILURE);
+        registerReceiver(broadcastReceiver, intentFilter);
 
         dbHelper = new CoinSummaryDbHelper(MainActivity.this);
         coinSummaryDatabase = dbHelper.getWritableDatabase();
@@ -92,63 +93,13 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         coinSummaryDatabase.close();
         dbHelper.close();
+        unregisterReceiver(broadcastReceiver);
         super.onDestroy();
     }
 
     private void requestDataFromCoinMarketCap() {
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(COIN_MARKET_CAP_API_URL, new AsyncHttpResponseHandler() {
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                String data = new String(responseBody);
-                JsonElement jsonElement = new JsonParser().parse(data);
-                JsonArray baseArray = jsonElement.getAsJsonArray();
-
-                for(JsonElement coinDataElement : baseArray) {
-
-                    JsonObject coinData = coinDataElement.getAsJsonObject();
-                    String symbol = coinData.get("symbol").getAsString();
-                    String name = coinData.get("name").getAsString();
-                    String id = coinData.get("id").getAsString();
-                    int rank = coinData.get("rank").getAsInt();
-                    String priceUSD = coinData.get("price_usd").getAsString();
-
-                    // Query existing data
-                    String selection = CoinSummarySchema.CoinEntry.COLUMN_NAME_SYMBOL + " = ?";
-                    String selectionArgs[] = { symbol };
-                    Cursor cursor = coinSummaryDatabase.query(
-                            CoinSummarySchema.CoinEntry.TABLE_NAME,
-                            CoinSummarySchema.allProjection,
-                            selection,
-                            selectionArgs,
-                            null,
-                            null,
-                            null
-                    );
-
-                    if (cursor.moveToNext()) {
-                        CoinSummary summary = CoinSummary.buildFromCursor(cursor);
-                        summary.setPriceUSD(new BigDecimal(priceUSD));
-                        summary.setRank(rank);
-                        summary.updateDatabase(coinSummaryDatabase, "price", "rank");
-                    } else {
-                        CoinSummary summary = new CoinSummary(symbol, name, id);
-                        summary.setPriceUSD(new BigDecimal(priceUSD));
-                        summary.setRank(rank);
-                        summary.addToDatabase(coinSummaryDatabase);
-                    }
-                }
-                initialiseCoinSummaryList();
-                swipeRefreshLayout.setRefreshing(false);
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                swipeRefreshLayout.setRefreshing(false);
-                Toast.makeText(getBaseContext(), getString(R.string.network_update_failed), Toast.LENGTH_SHORT).show();
-            }
-        });
+        Intent intent = new Intent(this, CoinMarketCapUpdaterService.class);
+        startService(intent);
     }
 
     private void initialiseCoinSummaryList() {
@@ -257,4 +208,20 @@ public class MainActivity extends Activity {
         intent.putExtra("coinSummary", summary);
         startActivity(intent);
     }
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case MAIN_ACTIVITY_REFRESH:
+                    MainActivity.this.initialiseCoinSummaryList();
+                    swipeRefreshLayout.setRefreshing(false);
+                    break;
+                case CoinMarketCapUpdaterService.STATUS_FAILURE:
+                    Toast.makeText(getBaseContext(), getString(R.string.network_update_failed), Toast.LENGTH_SHORT).show();
+                    swipeRefreshLayout.setRefreshing(false);
+                    break;
+            }
+        }
+    };
 }
