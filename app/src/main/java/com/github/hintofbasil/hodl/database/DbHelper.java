04f05b62687c727d889d01dbdee678dacd9bca78
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2017-09-07 17:55:29 +0100

    Added exchange rate table to DbHelper

--- app/src/main/java/com/github/hintofbasil/hodl/database/DbHelper.java
+++ app/src/main/java/com/github/hintofbasil/hodl/database/DbHelper.java
@@ -5,14 +5,19 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.github.hintofbasil.hodl.database.schemas.CoinSummarySchema;
+import com.github.hintofbasil.hodl.database.schemas.ExchangeRateSchema;
 
 /**
  * Created by will on 8/21/17.
+ *
+ *  Version 1: Create coin_summary table
+ *  Version 2: Create exchange_rate table
+ *
  */
 
 public class DbHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "CoinSummary.db";
 
     public DbHelper(Context context) {
@@ -22,10 +27,13 @@ public class DbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(CoinSummarySchema.SQL_CREATE_ENTRIES);
+        sqLiteDatabase.execSQL(ExchangeRateSchema.SQL_CREATE_ENTRIES);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        if (oldVersion < 2) {
+            sqLiteDatabase.execSQL(ExchangeRateSchema.SQL_CREATE_ENTRIES);
+        }
     }
 }
