7918ca06c0d3ab46d031532344b33c4629f39572
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2017-09-10 15:29:20 +0100

    Initiaised USD exchange rate on database creation

--- app/src/main/java/com/github/hintofbasil/hodl/database/DbHelper.java
+++ app/src/main/java/com/github/hintofbasil/hodl/database/DbHelper.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.github.hintofbasil.hodl.database.objects.ExchangeRate;
 import com.github.hintofbasil.hodl.database.schemas.CoinSummarySchema;
 import com.github.hintofbasil.hodl.database.schemas.ExchangeRateSchema;
+
+import java.math.BigDecimal;
 
 /**
  * Created by will on 8/21/17.
@@ -26,14 +29,21 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(CoinSummarySchema.SQL_CREATE_ENTRIES);
-        sqLiteDatabase.execSQL(ExchangeRateSchema.SQL_CREATE_ENTRIES);
+        onUpgrade(sqLiteDatabase, 0, DATABASE_VERSION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        if (oldVersion < 1) {
+            sqLiteDatabase.execSQL(CoinSummarySchema.SQL_CREATE_ENTRIES);
+        }
         if (oldVersion < 2) {
             sqLiteDatabase.execSQL(ExchangeRateSchema.SQL_CREATE_ENTRIES);
+
+            // Insert USD with exchange rate of 1
+            // USD is never returned from fixer.io as it is the base rate
+            ExchangeRate usdExchangeRate = new ExchangeRate("USD", new BigDecimal(1));
+            usdExchangeRate.addToDatabase(sqLiteDatabase);
         }
     }
 }
